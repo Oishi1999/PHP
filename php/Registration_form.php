1d0c37b263da5e275bbbd5ed97cd5edf47a1feaf
oishi1999 <17-34921-@student.aiub.edu>
2020-11-15 12:40:14 +0600

    done

--- php/Registration_form.php
+++ php/Registration_form.php
@@ -1,11 +1,68 @@
+<?php
+$gender="";
+$err_gender="";
+$hobbies="";
+$err_hobbies="";
+$profession="";
+$err_profession="";
+$name="";
+$err_name="";
+
+$has_error=false;
+
+
+if(isset ($_post["register"])){
+	if(empty($_Post[name])){
+		$err_name="name required";
+		$has_error=true;
+	}
+	else{
+		$name= $_Post["name"];
+	}
+}
+{
+	if(!isset($_Post["gender"])){
+		$err_gender="gender required";
+		$has_error=true;
+		 }
+	else{
+		$gender= $_Post["gender"];
+	}
+     if(!isset($_Post["hobbies"])){
+		 $err_hobbies="AT least select 1 hobby";
+		 $has_error=true;
+		 
+		 
+	 }
+	 else{
+		 $hobbies=$_Post("hobbies");
+	 }
+}
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
 <html>
     <head>
 	   <title>Registration</title>
 	   
 	</head>   
             <body>
-			 <h1>Registration form</h1>
-			    <form>
+			 
+			    <form  action="" method="post">
+				<fieldset>
+				<legend>Registration form</legend>
 			  
 			      <table border="1">
 				    <tr>
@@ -33,8 +90,8 @@
 					   <td>Gender:</td>
 					        <td>
 					  
-					         <input type="radio" name="Gender"> Male
-						      <input type="radio" name="Gender"> Female
+					         <input type="radio" name="Gender" value="Male"> Male
+						      <input type="radio" name="Gender" value="Female"> Female
 						
 						    </td>
 					
@@ -47,26 +104,26 @@
 					   <td>Hobbies:</td>
 					        <td>
 					  
-					         <input type="checkbox" > swimming <br>
-						     <input type="checkbox"> reading<br>
-							 <input type="checkbox"> programming<br>
-							 <input type="checkbox"> movies
+					         <input type="checkbox" name="Hobbies[]" value="swimming"> swimming <br>
+						     <input type="checkbox" name="Hobbies[]" value="reading"> reading<br>
+							 <input type="checkbox" name="Hobbies[]" value="programming"> programming<br>
+							 <input type="checkbox" name="Hobbies[]" value="movies"> movies<br>
 						
 						    </td>
 					
 					</tr>
 					<tr>
 					
-					   <td>Professions:</td>
+					   <td>Profession:</td>
 					        <td>
 					  
-					         <select>
+					         <select name="profession">
 							    <option disabled="disabled" selected="selected">choose</option>
-							    <option>Teacher</option>
-								<option>Doctor</option>
-								<option>Enginnere</option>
-								<option>Govt Officer</option>
-								<option>Banking</option>
+							    <option value="1">Teacher</option>
+								<option value="2">Doctor</option>
+								<option value="3">Enginnere</option>
+								<option value="4">Govt Officer</option>
+								<option value="5">Banking</option>
 							 
 							 </select>
 						
@@ -79,7 +136,7 @@
 					   <td>Bio:</td>
 					        <td>
 					  
-					         <textarea></textarea>
+					         <textarea name="bio"></textarea>
 						
 						    </td>
 					
@@ -87,15 +144,15 @@
 					<tr>
 					
 					   <td colspan="3" align="center">
-					   <input type= "submit" value="register">
+					   <input type= "submit" name="register" value="register">
 					   </td>
 					        
 					
 					</tr>
-					
+					</table>
 		 
-		 
-		 
+		 </fieldset>
+		 </form>
 		    </body>
   
    
