922b1349480bd8ed54c02f82672ab7ff6eece9c8
oishi1999 <17-34921-@student.aiub.edu>
2020-11-19 14:17:34 +0600

    done

--- php/Loginnn.php
+++ php/Loginnn.php
@@ -8,23 +8,23 @@
         <center>
           <h1>login form</h1>
 	    
-	         <form>
+	         <form action="loginnn_submitted.php" method="POST" >
 			     <table border="1">
 				    <tr>
 					  <td>username:</td>
-					  <td><input type="text" placeholder="write your user name"></td>
+					  <td><input type="text" name="username" placeholder="write your user name"></td>
 					
 					</tr>
 					
 					<tr>
 					  <td>password:</td>
-					  <td> <input type="password"></td>
+					  <td> <input  name="password" type="password"></td>
 					
 					</tr>
 					<tr>
 					  
 					  <td colspan="3" align="center">
-					  <a href="Registration page.html" target="_blank">Not registered yet?</a> &nbsp;
+					  <a href="Registration.html" target="_blank">Not Reg yet?</a> &nbsp; &nbsp; &nbsp
 					  <input type= "submit" value="login"></td>
 					
 					</tr>
---
+++ php/loginnn_submitted.php
@@ -0,0 +1,25 @@
+<html>
+
+ <head></head>
+    <body>
+	 <h1>login form submitted</h1>
+	 <?php
+	    echo "username :" . $_REQUEST["username"]. "<br>";
+		echo "password :" . $_REQUEST["password"]. "<br>";
+		
+	 
+	 
+	 
+	 
+	 ?>
+	 
+  
+  
+    </body>
+
+
+
+
+
+
+</html>
